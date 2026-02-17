74df08f9070647fa56c8f0a1168c5a6c84099b17
Max Longton <enrique.longton@gmail.com>
2026-02-17 07:15:25 -0500

    Add subtitle to App component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div style={{ fontFamily: "system-ui", padding: 24 }}>
       <h1>Made a change baby - let's see</h1>
+      <h2>Here's another change 02/17</h2>
       <p>Web is up. API base: {apiBase}</p>
     </div>
   );
